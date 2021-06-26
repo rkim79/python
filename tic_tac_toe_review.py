4b55ccbb74a5b04fa264183819ede71f3418b3e8
richard <phrozensea@gmail.com>
2021-06-26 23:28:27 +0900

    class concept refactoring

--- tic_tac_toe_review.py
+++ tic_tac_toe_review.py
@@ -1,18 +1,45 @@
 from random import randint
 
-# display game board
-def display_board(board):
-    print('   |   |')
-    print(f' {board[7]} | {board[8]} | {board[9]}')
-    print('   |   |')
-    print('------------')
-    print('   |   |')
-    print(f' {board[4]} | {board[5]} | {board[6]}')
-    print('   |   |')
-    print('------------')
-    print('   |   |')
-    print(f' {board[1]} | {board[2]} | {board[3]}')
-    print('   |   |')
+class Board:
+    def __init__(self):
+        self.board = [str(i) for i in range(10)] # not sure about self.board
+
+    def display_board(self):
+        print('   |   |')
+        print(f' {self.board[7]} | {self.board[8]} | {self.board[9]}')
+        print('   |   |')
+        print('------------')
+        print('   |   |')
+        print(f' {self.board[4]} | {self.board[5]} | {self.board[6]}')
+        print('   |   |')
+        print('------------')
+        print('   |   |')
+        print(f' {self.board[1]} | {self.board[2]} | {self.board[3]}')
+        print('   |   |')
+
+    def mark(self, position, marker):
+        if self.board[position].isdigit():
+            self.board[position] = marker
+            return True
+        else:
+            return False
+
+    def win_check(self, marker):
+        if ((self.board[1] == self.board[2] == self.board[3] == marker) or
+            (self.board[4] == self.board[5] == self.board[6] == marker) or
+            (self.board[7] == self.board[8] == self.board[9] == marker) or
+            (self.board[1] == self.board[4] == self.board[7] == marker) or
+            (self.board[2] == self.board[5] == self.board[8] == marker) or
+            (self.board[3] == self.board[6] == self.board[9] == marker) or
+            (self.board[1] == self.board[5] == self.board[9] == marker) or
+            (self.board[3] == self.board[5] == self.board[7] == marker)):
+            return marker 
+
+    def full_board_check(self):
+        for cell in self.board[1:]:
+            if cell.isdigit():
+                return False
+        return True
 
 # select marker for each player
 def marker_choice():
@@ -39,10 +66,6 @@ def random_first():
     else:
         return 'player2'
 
-# check if the place is empty
-def vacant_check(board, position):
-    return board[position].isdigit()
-
 # place marker on the board
 def make_move(board, player_marker, turn):
     while True:
@@ -56,42 +79,24 @@ def make_move(board, player_marker, turn):
                 print('Out of range, try again...')
                 continue
             else:
-                if vacant_check(board, position):
-                    board[position] = player_marker
+                if board.mark(position, player_marker):
                     break
                 else:
                     print(f'{position} is already occupied, try again...')
                     continue
 
-# check if the board is full which means the game is a draw
-def full_board_check(board):
-    for i in range(1, 10):
-        if vacant_check(board, i):
-            return False
-    return True
-
-# check if someone wins the game
-def win_check(b, marker):
-    if ((b[1] == b[2] == b[3] == marker) or (b[4] == b[5] == b[6] == marker) or
-        (b[7] == b[8] == b[9] == marker) or (b[1] == b[4] == b[7] == marker) or
-        (b[2] == b[5] == b[8] == marker) or (b[3] == b[6] == b[9] == marker) or
-        (b[1] == b[5] == b[9] == marker) or (b[3] == b[5] == b[7] == marker)):
-        return marker
-        # multiple line statement can be written with parentheses
-        # example a = (b
-        #               + c)
-
 # game function
 def game(board, player, turn):
     make_move(board, player, turn)
 
-    display_board(board)
+    board.display_board()
+    print(board.full_board_check())
 
-    if win_check(board, player) == player:
+    if board.win_check(player) == player:
         print(f'{turn} has won the game!')
         return (False, turn)
-    elif full_board_check(board):
-        print('No more space, Draw!')
+    elif board.full_board_check():
+        print('No more space! Draw!')
         return (False, turn)
     else:
         if turn == 'player1':
@@ -115,10 +120,10 @@ def wanna_replay():
 while True:
     print('Welcome to tic-tac-toe game!\n')
 
-    board = [str(cell) for cell in range(10)]
+    new_board = Board()
     game_on = True
 
-    display_board(board)
+    new_board.display_board()
 
     player1, player2 = marker_choice()
 
@@ -128,9 +133,9 @@ while True:
 
     while game_on:
         if turn == 'player1':
-            game_on, turn = game(board, player1, turn)
+            game_on, turn = game(new_board, player1, turn)
         elif turn == 'player2':
-            game_on, turn = game(board, player2, turn)
+            game_on, turn = game(new_board, player2, turn)
 
     # replay
     if wanna_replay():
